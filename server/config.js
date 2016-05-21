b49e9c52f7812fc34624bc6985e4fe1d30d2702e
hilongjw <hilongjw@qq.com>
2016-05-21 21:34:16 +0800

    增加预览图

--- server/config.js
+++ server/config.js
@@ -1,12 +1,11 @@
 const AV = require('leanengine')
 
 const config = {
-    appId: 'Myg6wSTVY6xWcg845dkF4rpe-gzGzoHsz',
-    appKey: 'hOD58ye6VmgYn36q8MUwzRBP',
-    masterKey: 'Dj1jOtxsp2XuUQp2m8cDWx1X'
+    appId: 'dpeFsmm4JIVgnmQP0tizLCcO-gzGzoHsz',
+    appKey: '2ph8iVVCL1TtNjKNCUG8TtKn'
 }
 
-AV.initialize(config.appId, config.appKey, config.masterKey)
+AV.initialize(config.appId, config.appKey)
 AV.Cloud.useMasterKey()
 
 exports.AV = AV
